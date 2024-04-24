7fc7c2d10b8b63882c4ae480471959c75d257bcf
pennyhc <135057571+pennyhc@users.noreply.github.com>
2024-04-24 11:14:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,14 @@ def main():
     tan_V=psi_2_V(psi)
     horizontal,vertical=trig(angle,tan_V)
 
+def time(vertical, g=-10):
+    coeff = [g/2, vertical]
+    times = np.roots(coeff)
+    return max(times)
+
+def range(time, horizontal):
+    range = time*horizontal
+    return range
+
 if __name__ == "__main__":
         main()
