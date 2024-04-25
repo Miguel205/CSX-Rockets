64128c7f8f81e7c6699831b09b3447c521c75a62
pennyhc <135057571+pennyhc@users.noreply.github.com>
2024-04-25 14:24:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import numpy as np
 def psi_2_V(psi):
     V=.188*psi+8.72     #slope of exel graph
     return V
+
 def trig(angle,V):
-    horizontal_v=np.cos(angle)*V
+    angle = np.radians(int(angle))
+    horizontal_v=round(np.cos(angle))*V
     vertical_v=np.sin(angle)*V
     return [horizontal_v,vertical_v]
 
@@ -13,6 +15,11 @@ def main():
     angle=int(input("launch angle\n"))
     tan_V=psi_2_V(psi)
     horizontal,vertical=trig(angle,tan_V)
+    t = time(vertical)
+    r = range(t, horizontal)
+
+    print("launch velocity", f"{tan_V}ft/s", "time", f"{t}sec", " range", f"{r}ft")
+    
 
 def time(vertical, g=-10):
     coeff = [g/2, vertical]
@@ -22,6 +29,7 @@ def time(vertical, g=-10):
 def range(time, horizontal):
     range = time*horizontal
     return range
+     
 
 if __name__ == "__main__":
         main()
